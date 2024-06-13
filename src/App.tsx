15fe32814ab762d2e3bb00df5ad458c22ebfb095
Simon Babatunde <orlhatundji@gmail.com>
2024-06-13 13:05:40 +0100

    fix gh-pages 404 on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const About = lazy(() => import('./About'));
-const Home = lazy(() => import('./Home'));
+const About = lazy(() => import("./About"));
+const Home = lazy(() => import("./Home"));
 
 const App: React.FC = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
-    <nav>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/about" element={<About />}>
-      </Route>
-      <Route path="/" element={<Home />}>
-      </Route>
-    </Routes>
+      <nav>
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">-Home-</Link>
+          </li>
+          <li>
+            <Link to="/about">-About-</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/patient-telemedicine-ehr/about" element={<About />}></Route>
+        <Route path="/patient-telemedicine-ehr/" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </Suspense>
   </Router>
 );
